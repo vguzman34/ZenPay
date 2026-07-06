56f87ec391297dc333c68a680f666b47631a54f0
Vanesa <vanesa@zenpay.app>
2026-07-06 10:25:38 -0500

    chore: clean up temp test files

--- gen_hash.js
+++
@@ -1,5 +0,0 @@
-const bcrypt = require('bcryptjs');
-const salt = bcrypt.genSaltSync(10);
-const hash = bcrypt.hashSync('Admin123', salt);
-console.log('Hash for Admin123:', hash);
-console.log('Verify:', bcrypt.compareSync('Admin123', hash));
--- verify_hash.js
+++
@@ -1,4 +0,0 @@
-const bcrypt = require('bcryptjs');
-const conversationHash = '$2b$10$TyADFHRlFFU6NT9q3313v.2K5uqHqKjJxgYs4s4dKyItLXFjnydJm';
-console.log('Conversation hash matches Admin123:', bcrypt.compareSync('Admin123', conversationHash));
-console.log('Conversation hash matches admin123:', bcrypt.compareSync('admin123', conversationHash));
